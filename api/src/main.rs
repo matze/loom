7e0662967eb610829d717c8922da372834f360fc
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2022-01-20 22:31:16 +0100

    Initial commit

---
+++ api/src/main.rs
@@ -0,0 +1,76 @@
+use anyhow::Result;
+use axum::extract::Path;
+use axum::headers::{HeaderMap, HeaderValue};
+use axum::http::header::CONTENT_TYPE;
+use axum::http::StatusCode;
+use axum::response::Html;
+use axum::routing::get;
+use axum::Router;
+use include_dir::{include_dir, Dir};
+use tracing::{instrument, warn};
+
+static DIST_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/../app/dist");
+
+fn insert_header_from_extension(map: &mut HeaderMap, ext: &str) {
+    match ext {
+        "wasm" => {
+            map.insert(CONTENT_TYPE, HeaderValue::from_static("application/wasm"));
+        }
+        "js" => {
+            map.insert(
+                CONTENT_TYPE,
+                HeaderValue::from_static("application/javascript"),
+            );
+        }
+        _ => {}
+    }
+}
+
+#[instrument]
+async fn get_static(Path(path): Path<String>) -> (StatusCode, HeaderMap, Vec<u8>) {
+    let mut headers = HeaderMap::new();
+
+    match DIST_DIR.get_file(&path) {
+        Some(file) => {
+            file.path()
+                .extension()
+                .map(|e| e.to_str())
+                .flatten()
+                .map(|e| insert_header_from_extension(&mut headers, e));
+
+            (StatusCode::OK, headers, file.contents().to_vec())
+        }
+        None => {
+            warn!("file not found");
+            (StatusCode::NOT_FOUND, headers, vec![])
+        }
+    }
+}
+
+#[instrument]
+async fn index() -> Html<String> {
+    Html(
+        DIST_DIR
+            .get_file("index.html")
+            .expect("index.html not found")
+            .contents_utf8()
+            .expect("index.html is not UTF-8 encoded")
+            .to_string(),
+    )
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/:key", get(get_static));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse()?)
+        .serve(app.into_make_service())
+        .await?;
+
+
+    Ok(())
+}

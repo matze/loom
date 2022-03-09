f32956bd330386b18103535d46063683ad6c3171
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2022-03-09 23:54:06 +0100

    Add JWT-based authorization endpoint

--- src/error.rs
+++ src/error.rs
@@ -10,12 +10,26 @@ pub(crate) enum Error {
     TimeError(#[from] time::error::Format),
     #[error("Database problem: {0}")]
     SqlError(#[from] sqlx::Error),
+    #[error("Missing credentials")]
+    MissingCredentials,
+    #[error("Wrong credentials")]
+    WrongCredentials,
+    #[error("JWT creation problem: {0}")]
+    TokenGeneration(#[from] jsonwebtoken::errors::Error),
+    #[error("Invalid token")]
+    InvalidToken,
 }
 
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
+        let status = match self {
+            Error::MissingCredentials => StatusCode::BAD_REQUEST,
+            Error::WrongCredentials => StatusCode::UNAUTHORIZED,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+
         Response::builder()
-            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .status(status)
             .body(body::boxed(body::Full::from(format!("Error: {}", self))))
             .unwrap()
     }
